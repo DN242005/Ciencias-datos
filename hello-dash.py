900062bfe087230d952de75664189c184cf488b1
jonathan daniel <zS23020598@estudiantes.uv.mx>
2026-09-02 03:12:16 +0000

    “cambiouno”

--- hello-dash.py
+++ hello-dash.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd 
-st.title("hello streamlit")
+st.title("Jonathan")
+st.title("hello world web")
+st.write("hello world streamlit")
 dataframe = pd.read_csv("https://raw.githubusercontent.com/adsoftsito/ciencia-datos/refs/heads/main/titanic.csv")
 st.dataframe(dataframe)
 st.write("by adsoftsito")
