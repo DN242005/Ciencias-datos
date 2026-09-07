bb2dcdc2c553123c55fac6162aef96c329368a09
jonathan daniel <zS23020598@estudiantes.uv.mx>
2026-09-07 04:19:16 +0000

    Dashboard de autos terminado con Streamlit

--- hello-dash.py
+++ hello-dash.py
@@ -1,8 +1,14 @@
 import streamlit as st
-import pandas as pd 
-st.title("Jonathan")
-st.title("hello world web")
-st.write("hello world streamlit")
-dataframe = pd.read_csv("https://raw.githubusercontent.com/adsoftsito/ciencia-datos/refs/heads/main/titanic.csv")
+import pandas as pd
+
+st.title("Dashboard de Autos - Ciencia de Datos")
+st.write("Visualización de datos del dataset Cars93")
+
+
+url = "https://raw.githubusercontent.com/DN242005/Ciencias-datos/main/Cars93.csv"
+dataframe = pd.read_csv(url)
+
+
 st.dataframe(dataframe)
-st.write("by adsoftsito")
+
+st.bar_chart(dataframe["Price"])
